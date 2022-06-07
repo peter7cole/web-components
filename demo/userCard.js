a5d6e951d183523ebc8ea7fd6fc77f86cbd2145c
peter-robert-cole <peter7cole@gmail.com>
2022-06-06 23:12:52 -0700

    feat(style): styled cards and images

--- demo/userCard.js
+++ demo/userCard.js
@@ -1,11 +1,45 @@
 const template = document.createElement('template');
+
 template.innerHTML = `
   <style>
-    h3 { 
+    h2 { 
       color: var(--green);
     }
+    .user-card {
+      font-family: Arial, sans-serif;
+      background: #f4f4f4;
+      width: 500px;
+      display: grid;
+      grid-template-columns: 1fr 2fr;
+      gap: 10px;
+      margin-bottom: 15px;
+      border-bottom: 5px solid var(--green);
+    }
+    
+    .user-card img {
+      width: 100%;
+    }
+    
+    .user-card button {
+      cursor: pointer;
+      background-color: var(--green);
+      color: var(--white);
+      border: none;
+      border-radius: 5px;
+      padding: 5px 10px;
+    }
   </style>
-  <h3></h3>
+  <div class="user-card">
+    <img />
+    <div>
+      <h2></h2>
+      <div>
+        <p>Phone</p>
+        <p>Email</p>
+      </div>
+      <button id="toggle-info">Hide Info</button>
+    </div>
+  </div>
 `;
 
 class UserCard extends HTMLElement {
@@ -17,7 +51,11 @@ class UserCard extends HTMLElement {
     this.shadowRoot.appendChild(template.content.cloneNode(true));
 
     // set up the name attribute
-    this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name');
+    this.shadowRoot.querySelector('h2').innerText = this.getAttribute('name');
+    // set up the avatar attribute
+    this.shadowRoot.querySelector('img').src = this.hasAttribute('avatar')
+      ? this.getAttribute('avatar')
+      : 'https://http.cat/404';
   }
 }
 
